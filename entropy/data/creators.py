83ad8e0fe996ef206555d9b1fdc15f777ed6277b
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-09-15 11:56:02 +0100

    scaled incomes

--- entropy/data/creators.py
+++ entropy/data/creators.py
@@ -20,9 +20,10 @@ def calc_balance(df):
     """Calculate running account balance.
 
     Daily account balance is calculated as the sum of the cumulative
-    balance and the starting balance, which is the difference between
-    the cumulative balance and the actually reported balance on the
-    day of the last refresh.
+    balance and the starting balance, where the starting balance is
+    the difference between the cumulative balance and the actually
+    reported balance on the day of the last refresh or the nearest
+    preceeding date.
     """
     def helper(g):
         last_refresh_balance = g.latest_balance.iloc[0]
@@ -72,68 +73,3 @@ def calc_income(df):
     return merged.drop(columns='y')
 
 
-def calculate_salaries(df):
-    """Return monthly and yearly salary for each user.
-
-    Salaries are monthly/yearly aggregated sums of
-    all transactions tagged as salaries by MDB.
-    """
-    def make_groupers(df):
-        """Create user-year-month/user-year strings for merge."""
-        y = df.date.dt.to_period('Y').astype('str')
-        ym = df.date.dt.to_period('M').astype('str')
-        str_id = df.user_id.astype('str')
-        df['month_grouper'] = str_id + ' - ' + ym
-        df['year_grouper'] = str_id + ' - ' + y
-        return df
-
-    def calc_monthly_salary(df):
-        """Return monthly salary for each user."""
-        tagsum = df[['auto_tag', 'manual_tag', 'tag']].sum(1)
-        mask = tagsum.str.contains('salary')
-        monthly_salary = (
-            df[mask]
-            .set_index(['user_id', 'date'])
-            .groupby(level='user_id')
-            .resample('M', level='date')
-            .amount.sum().abs()
-            .reset_index()
-            .pipe(make_groupers)
-            .drop(['date', 'user_id', 'year_grouper'], axis=1)
-            .rename(columns={'amount': 'monthly_salary'})
-        )
-        return df.merge(monthly_salary, how='left', validate='m:1')
-
-    def calc_yearly_salary(df):
-        """Return yearly salary for each user."""
-        tagsum = df[['auto_tag', 'manual_tag', 'tag']].sum(1)
-        mask = tagsum.str.contains('salary')
-        yearly_salary = (
-            df[mask]
-            .set_index(['user_id', 'date'])
-            .groupby(level='user_id')
-            .resample('Y', level='date')
-            .amount.sum().abs()
-            .reset_index()
-            .pipe(make_groupers)
-            .drop(['date', 'user_id', 'month_grouper'], axis=1)
-            .rename(columns={'amount': 'yearly_salary'})
-        )
-        return df.merge(yearly_salary, how='left', validate='m:1')
-
-    def drop_groupers(df):
-        return df.drop(['month_grouper', 'year_grouper'], axis=1)
-
-    def replace_nans(df):
-        df['monthly_salary'] = df.monthly_salary.fillna(0)
-        df['yearly_salary'] = df.yearly_salary.fillna(0)
-        return df
-
-    return (
-        df
-        .pipe(make_groupers)
-        .pipe(calc_monthly_salary)
-        .pipe(calc_yearly_salary)
-        .pipe(drop_groupers)
-        .pipe(replace_nans)
-    )
