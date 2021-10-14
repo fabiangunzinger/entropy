d3de50d49dbe3494167df031681b71e87485efe9
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-10-14 18:14:10 +0100

    first simple test

--- tests/data/test_sample.py
+++ tests/data/test_sample.py
@@ -1,18 +1,26 @@
 import pandas as pd
 import pytest
 
-from ..entropy.data import cleaners
-
-def test_drop_last_month_passes(df):
-    data = cleaners.drop_last_month(df)
-    assert data.date.dt.month.max() == 2
+from entropy.data import cleaners
+import fixtures
 
 
+def test_drop_last_month_passes():
 
+    df = pd.DataFrame({
+        'date': pd.to_datetime(['2020-01-11', '2020-02-12', '2020-03-13']),
+        'amount': [11.1, 22.2, 33.3],
+        'desc': ['costa coffee', 'waitrose', 'pure gym']
+    })
 
-def test_df(mdb_data):
-    assert len(mdb_data.columns) == 27
+    actual = cleaners.drop_last_month(df)
 
+    expected = pd.DataFrame({
+        'date': pd.to_datetime(['2020-01-11', '2020-02-12']),
+        'amount': [11.1, 22.2],
+        'desc': ['costa coffee', 'waitrose']
+    })
 
+    pd.testing.assert_frame_equal(actual, expected)
 
 
