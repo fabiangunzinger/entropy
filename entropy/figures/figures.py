fdac63843020414ac981acd3e70b5e40f45f4c1f
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-10-14 15:53:37 +0100

    use helper functions

--- entropy/figures/figures.py
+++ entropy/figures/figures.py
@@ -13,15 +13,29 @@ import seaborn as sns
 from entropy import config
 
 
+def _set_style():
+    plt.style.use(['seaborn-colorblind', 'seaborn-whitegrid'])
+
+
+def _save_fig(fig, name):
+    DPI = 1200
+    fp = os.path.join(config.FIGDIR, name)
+    fig.savefig(fp, dpi=DPI)
+    print(f'{name} written.')
+
+    
 def income_distribution(df):
     """Plots histogram of annual incomes."""
     incomes = df.groupby(['user_id', df.date.dt.year]).income.first()
     fig = sns.displot(incomes, aspect=2)
-    fp = os.path.join(config.FIGDIR, 'income_distribution.png')
-    plt.savefig(fp)
+    # fp = os.path.join(config.FIGDIR, 'income_distribution.png')
+    # plt.savefig(fp)
+
+    _save_fig(fig, 'income_distribution.png')
 
 
-def balances_by_account_type(data, write=True):
+
+def balances_by_account_type(df, write=True):
 
     def make_data(df, account_type='current', freq='w',
                   agg='median', start=None, end=None):
@@ -43,48 +57,37 @@ def balances_by_account_type(data, write=True):
                 .resample(freq).agg(agg)
                 .loc[start:end])
 
-    def draw_plot(data):
+    def draw_plot(df):
         linestyles = [k for k, v in mpl.lines.lineStyles.items()]
         markers = [k for k, v in mpl.markers.MarkerStyle.markers.items()]
         fig, ax = plt.subplots()
-        for i, col in enumerate(data):
-            ax.plot(data[col], label=col, linestyle=linestyles[i])
-        ax.legend(title=data.columns.name)
+        for i, col in enumerate(df):
+            ax.plot(df[col], label=col, linestyle=linestyles[i])
+        ax.legend(title=df.columns.name)
         return fig, ax
 
     def set_labels(ax):
         ax.set_xlabel('Year')
         ax.set_ylabel('Median account balance (£)')
 
-    def set_style():
-        plt.style.use(['seaborn-colorblind'])
-
     def set_size(fig):
         fig.set_size_inches(8, 5)
         fig.tight_layout()
 
-    def save_fig(fig, name):
-        DPI = 1200
-        fp = os.path.join(config.FIGDIR, name)
-        fig.savefig(fp, dpi=DPI)
-        print(f'{name} written.')
 
-        
-    types = ['current', 'savings']
-    data = make_data(data, account_type=types, start='2015')    
-
-    set_style()
+    data = make_data(df, account_type=['current', 'savings'], start='2015')    
     fig, ax = draw_plot(data)
     set_labels(ax)
     set_size(fig)
     if write: 
-        save_fig(fig, 'balances_by_account_type.png')
+        _save_fig(fig, 'balances_by_account_type.png')
 
 
 if __name__ == '__main__':
 
     data = pd.read_parquet('~/tmp/entropy_X77.parquet')
 
+    _set_style()
     income_distribution(data)
     balances_by_account_type(data)
     
