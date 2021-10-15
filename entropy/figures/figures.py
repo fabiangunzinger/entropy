1146dd2c130390d324411a7f59314289914130c1
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-10-15 11:07:51 +0100

    txns by account type

--- entropy/figures/figures.py
+++ entropy/figures/figures.py
@@ -1,5 +1,11 @@
 """
-Create appendix figures.
+Create all figures.
+
+Approach:
+1. Load main dataset
+2. One function per figure, which produces data used in figure and figure
+itself.
+3. Use styling helpers across functions whenever possible.
 
 """
 
@@ -15,6 +21,11 @@ from entropy import config
 
 def _set_style():
     plt.style.use(['seaborn-colorblind', 'seaborn-whitegrid'])
+
+
+def _set_size(fig):
+    fig.set_size_inches(8, 5)
+    fig.tight_layout()
 
 
 def _save_fig(fig, name):
@@ -70,24 +81,52 @@ def balances_by_account_type(df, write=True):
         ax.set_xlabel('Year')
         ax.set_ylabel('Median account balance (£)')
 
-    def set_size(fig):
-        fig.set_size_inches(8, 5)
-        fig.tight_layout()
-
-
     data = make_data(df, account_type=['current', 'savings'], start='2015')    
     fig, ax = draw_plot(data)
     set_labels(ax)
-    set_size(fig)
+    _set_size(fig)
     if write: 
         _save_fig(fig, 'balances_by_account_type.png')
 
 
+def monthly_txns_by_account_type(df, write=True):
+
+    def make_data(df):
+        return (df.loc[df.account_type.ne('other')]
+                .set_index('date')
+                .groupby(['account_type', 'account_id'], observed=True)
+                .resample('M').id.count()
+                .rename('num_txns')
+                .reset_index())
+
+    def make_plot(df):
+        fig, ax = plt.subplots()
+        ax = sns.boxenplot(data=df, x='num_txns', y='account_type')
+        return fig, ax
+
+    def set_labels(ax):
+        # capitalise first letter of and add 'account' suffix to ytick labels
+        to_label = lambda x: ' '.join([x[0].upper() + x[1:], 'accounts'])
+        ytick_labels = [to_label(i.get_text()) for i in ax.get_yticklabels()]
+        ax.set_yticklabels(ytick_labels)
+
+        ax.set_xlabel('Number of transactions per month')
+        ax.set_ylabel('')
+    
+    fig, ax = make_plot(make_data(df))
+    set_labels(ax)
+    _set_size(fig)
+    if write:
+        _save_fig(fig, 'monthly_txns_by_account_type.png')
+
+
+
 if __name__ == '__main__':
 
-    data = pd.read_parquet('~/tmp/entropy_X77.parquet')
+    df = pd.read_parquet('~/tmp/entropy_X77.parquet')
 
     _set_style()
-    income_distribution(data)
-    balances_by_account_type(data)
+    income_distribution(df)
+    balances_by_account_type(df)
+    monthly_txns_by_account_type(df)
     
