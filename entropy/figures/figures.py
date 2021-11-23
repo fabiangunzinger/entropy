0bd92307251b11434f5b9fea2bc306e7734a3d42
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-11-23 10:38:18 +0000

    edits

--- entropy/figures/figures.py
+++ entropy/figures/figures.py
@@ -22,7 +22,7 @@ from entropy import config
 
 
 def _set_style():
-    plt.style.use(['seaborn-colorblind', 'seaborn-whitegrid'])
+    plt.style.use(["seaborn-colorblind", "seaborn-whitegrid"])
 
 
 def _set_size(fig):
@@ -34,41 +34,37 @@ def _save_fig(fig, name):
     DPI = 1200
     fp = os.path.join(config.FIGDIR, name)
     fig.savefig(fp, dpi=DPI)
-    print(f'{name} written.')
+    print(f"{name} written.")
 
-    
+
 def income_distr(df):
     """Plots histogram of annual incomes."""
-    incomes = df.groupby(['user_id', df.date.dt.year]).income.first()
+    incomes = df.groupby(["user_id", df.date.dt.year]).income.first()
     fig = sns.displot(incomes, aspect=2)
-    _save_fig(fig, 'income_distr.png')
-
+    _save_fig(fig, "income_distr.png")
 
 
 def balances_by_account_type(df, write=True):
-
-    def make_data(
-        df,
-        account_type='current',
-        freq='w',
-        agg='median',
-        start=None,
-        end=None
-    ):
+    def make_data(df, account_type, freq="w", agg="median", start=None, end=None):
         """Returns user balances at specified freq by account type."""
-        return (df.loc[df.account_type.isin(account_type)]
-                # single daily balance for each account
-                .set_index('date')
-                .groupby(['account_type', 'account_id'])
-                .resample('d').balance.first()
-                # median daily balance per account type
-                .groupby(['date', 'account_type']).median()
-                # one column per account type with legend labels
-                .unstack()
-                .rename(columns=lambda x: ' '.join([x.title(), 'accounts']))
-                .rename_axis(columns=lambda x: x.replace('_', ' ').title())
-                .resample(freq).agg(agg)
-                .loc[start:end])
+        return (
+            df.loc[df.account_type.isin(account_type)]
+            # single daily balance for each account
+            .set_index("date")
+            .groupby(["account_type", "account_id"])
+            .resample("d")
+            .balance.first()
+            # median daily balance per account type
+            .groupby(["date", "account_type"])
+            .median()
+            # one column per account type with legend labels
+            .unstack()
+            .rename(columns=lambda x: " ".join([x.title(), "accounts"]))
+            .rename_axis(columns=lambda x: x.replace("_", " ").title())
+            .resample(freq)
+            .agg(agg)
+            .loc[start:end]
+        )
 
     def draw_plot(df):
         linestyles = [k for k, v in mpl.lines.lineStyles.items()]
@@ -80,74 +76,76 @@ def balances_by_account_type(df, write=True):
         return fig, ax
 
     def set_labels(ax):
-        ax.set_xlabel('Year')
-        ax.set_ylabel('Median account balance (£)')
+        ax.set_xlabel("Year")
+        ax.set_ylabel("Median account balance (£)")
 
-    data = make_data(df, account_type=['current', 'savings'], start='2015')    
+    data = make_data(df, account_type=["current", "savings"], start="2015")
     fig, ax = draw_plot(data)
     set_labels(ax)
     _set_size(fig)
-    if write: 
-        _save_fig(fig, 'balances_by_account_type.png')
+    if write:
+        _save_fig(fig, "balances_by_account_type.png")
 
 
 def monthly_txns_by_account_type(df, write=True):
-
     def make_data(df):
-        return (df.loc[df.account_type.ne('other')]
-                .set_index('date')
-                .groupby(['account_type', 'account_id'], observed=True)
-                .resample('M').id.count()
-                .rename('num_txns')
-                .reset_index())
+        return (
+            df.loc[df.account_type.ne("other")]
+            .set_index("date")
+            .groupby(["account_type", "account_id"], observed=True)
+            .resample("M")
+            .id.count()
+            .rename("num_txns")
+            .reset_index()
+        )
 
     def make_plot(df):
         fig, ax = plt.subplots()
-        ax = sns.boxenplot(data=df, x='num_txns', y='account_type')
+        ax = sns.boxenplot(data=df, x="num_txns", y="account_type")
         return fig, ax
 
     def set_labels(ax):
         # capitalise first letter of and add 'account' suffix to ytick labels
-        to_label = lambda x: ' '.join([x[0].upper() + x[1:], 'accounts'])
+        to_label = lambda x: " ".join([x[0].upper() + x[1:], "accounts"])
         ytick_labels = [to_label(i.get_text()) for i in ax.get_yticklabels()]
         ax.set_yticklabels(ytick_labels)
 
-        ax.set_xlabel('Number of transactions per month')
-        ax.set_ylabel('')
-    
+        ax.set_xlabel("Number of transactions per month")
+        ax.set_ylabel("")
+
     fig, ax = make_plot(make_data(df))
     set_labels(ax)
     _set_size(fig)
     if write:
-        _save_fig(fig, 'monthly_txns_by_account_type.png')
-
+        _save_fig(fig, "monthly_txns_by_account_type.png")
 
 
 def txns_distrs(df, write=True):
     """Plots distibutions of txns, spending categories, and entropy."""
+
     def make_txns_hist(g):
         data = g.id.count()
         histplot(data=data, bins=20, ax=ax[0])
-        ax[0].set(xlabel='Transactions', ylabel=ylabel)    
+        ax[0].set(xlabel="Transactions", ylabel=ylabel)
 
     def make_spend_cat_hist(g):
         data = g.tag.nunique()
         bins = np.arange(df.tag.nunique() + 1) - 0.5
         histplot(data=data, bins=bins, ax=ax[1])
-        ax[1].set(xlabel='Spending categories', ylabel=ylabel)
+        ax[1].set(xlabel="Spending categories", ylabel=ylabel)
 
     def make_entropy_hist(g):
         data = g.entropy_tag.first()
         histplot(data=data, bins=20, ax=ax[2])
-        ax[2].set(xlabel='Entropy', ylabel=ylabel)
+        ax[2].set(xlabel="Entropy", ylabel=ylabel)
 
     def set_size(fig):
         fig.set_size_inches(8, 2.5)
         fig.tight_layout()
 
-    g = df.set_index('date').groupby('user_id').resample('M')
-    histplot = functools.partial(sns.histplot, stat='percent')
-    ylabel = 'User-months (%)'
+    g = df.set_index("date").groupby("user_id").resample("M")
+    histplot = functools.partial(sns.histplot, stat="percent")
+    ylabel = "User-months (%)"
 
     fig, ax = plt.subplots(1, 3)
     make_txns_hist(g)
@@ -155,12 +153,12 @@ def txns_distrs(df, write=True):
     make_entropy_hist(g)
     set_size(fig)
     if write:
-        _save_fig(fig, 'entropy_distr.png')
+        _save_fig(fig, "entropy_distr.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    df = pd.read_parquet('~/tmp/entropy_X77.parquet')
+    df = pd.read_parquet("~/tmp/entropy/entropy_X77.parquet")
 
     _set_style()
     # income_distr(df)
