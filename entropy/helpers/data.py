703de36f85cd15e4a425c7cf981651183d9f6972
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-11-30 10:29:18 +0000

    use helpers for data loading

--- entropy/helpers/data.py
+++ entropy/helpers/data.py
@@ -1,5 +1,24 @@
 import numpy as np
 
+from IPython.display import display
+import entropy.helpers.aws as ha
+import entropy.helpers.helpers as hh
+
+
+def inspect(df, nrows=2):
+    print('({:,}, {})'.format(*df.shape))
+    display(df.head(nrows))
+    
+
+@hh.timer
+def read_data(sample):
+    fp = f's3://3di-project-entropy/entropy_{sample}.parquet'
+    return ha.read_parquet(fp)
+
+
+def load_samples(samples):
+    return (read_data(sample) for sample in samples)
+
 
 def trim(series, pct=1):
     """Replaces series values outside of specified percentile on both sides with nan."""
