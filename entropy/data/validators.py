982f1be5cb9c86347941c565fb4265b2018cc6d3
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-01-04 15:17:49 +0100

    remove validator

--- entropy/data/validators.py
+++ entropy/data/validators.py
@@ -17,19 +17,20 @@ def validator(func):
 @validator
 def tag_groups(df):
     """All occurring tag groups are valid."""
-    actual = set(df.tag_group.unique())
+    actual = set(df.tag_group.cat.categories)
     expected = set(tc.tag_groups.keys())
     assert actual <= expected
     return df
 
 
-@validator
-def spend_tag(df):
-    """All occurring spend tags are valid."""
-    actual = set(df[df.tag_group.eq('spend')].tag.unique())
-    expected = set(tc.spend_subgroups.keys())
-    assert actual <= expected
-    return df
+# @validator
+# def spend_tag(df):
+#     """All occurring spend tags are valid."""
+#     spend_txns = df[df.tag_group.eq('spend')]
+#     actual = set(spend_txns.tag.cat.categories)
+#     expected = set(tc.spend_subgroups.keys())
+#     assert actual <= expected
+#     return df
 
 
 
