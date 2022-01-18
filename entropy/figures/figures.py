e744d3af3bd62ed3a0f63300309bc1665a635587
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-01-18 18:01:20 +0000

    Monthly savings figure improvements

--- entropy/figures/figures.py
+++ entropy/figures/figures.py
@@ -10,11 +10,11 @@ import pandas as pd
 import seaborn as sns
 import s3fs
 
-from entropy import config
-from entropy.helpers import aws
-import entropy.helpers.data as ha
-from entropy.helpers import helpers
+import entropy.helpers.helpers as hh
+import entropy.helpers.aws as ha
+import entropy.helpers.data as hd
 import entropy.figures.helpers as fh
+
 
 
 def user_income_hist(df, write=True):
@@ -233,12 +233,36 @@ def txns_breakdowns_and_entropy(df, write=True):
         fh.save_fig(fig, "txns_breakdowns_and_entropy.png")
 
 
-if __name__ == "__main__":
-def main(argv=None):
-    if argv is None:
-        argv = sys.argv[1:]
-    args = parse_args(argv)
-    df = aws.read_parquet(args.filepath)
-    for figure in paper_figures:
-        figure(df)
-    main()
+def monthly_savings(df):
+    """Plots histograms of savings account flows."""
+    pct_histplot = functools.partial(sns.histplot, stat='percent')
+    columns = ['sa_scaled_inflows', 'sa_scaled_outflows', 'sa_scaled_net_inflows']
+    xlabels = {
+        "sa_scaled_outflows": "Outflows (% of monthly income)",
+        "sa_scaled_inflows": "Inflows (% of monthly income)",
+        "sa_scaled_net_inflows": "Net inflows (% of monthly income)",
+    }
+    ylabel = "User-months (%)"
+
+    fig, ax = plt.subplots(2, 3)
+    for i, col in enumerate(columns):
+        data = df[col].pipe(hd.trim, pct=1, how='both')
+        pct_histplot(x=data, ax=ax[0, i])
+        ax[0, i].set(xlabel="", ylabel=ylabel)
+        data = data[data != 0]
+        pct_histplot(x=data, ax=ax[1, i])
+        ax[1, i].set(xlabel=xlabels[col], ylabel=ylabel)
+    fh.set_style()
+    fh.set_size(fig, height=3)
+    return fig, ax
+
+
+# if __name__ == "__main__":
+# def main(argv=None):
+#     if argv is None:
+#         argv = sys.argv[1:]
+#     args = parse_args(argv)
+#     df = aws.read_parquet(args.filepath)
+#     for figure in paper_figures:
+#         figure(df)
+#     main()
--- entropy/figures/monthly_savings.py
+++ entropy/figures/monthly_savings.py
@@ -1,4 +1,5 @@
 import argparse
+import functools
 import sys
 
 import matplotlib.pyplot as plt
@@ -10,71 +11,32 @@ import entropy.helpers.data as hd
 import entropy.figures.helpers as fh
 
 
-def make_data(df, trim_pct=5):
-    """Aggregates df into inflows, outflows, and net, by user month, trims at
-    specified percentile, and scales flows by user's monthly income.
-    """
-
-    def trim_column_values(df, **kwargs):
-        return df.apply(hd.trim, **kwargs)
-
-    df = df.copy()
-    is_not_interest_txn = ~df.tag_auto.str.contains("interest", na=False)
-    is_savings_account = df.account_type.eq("savings")
-    mask = is_not_interest_txn & is_savings_account
-    df["debit"] = df.debit.replace({True: "sa_outflows", False: "sa_inflows"})
-
-    return (
-        df[mask]
-        .groupby(["user_id", "ym", "income", "debit"])
-        .amount.sum()
-        .abs()
-        .unstack()
-        .fillna(0)
-        .reset_index("income")
-        .assign(
-            sa_inflows=lambda df: df.sa_inflows / (df.income / 12) * 100,
-            sa_outflows=lambda df: df.sa_outflows / (df.income / 12) * 100,
-            sa_net_inflows=lambda df: df.sa_inflows - df.sa_outflows,
-        )
-        .drop(columns="income")
-        .pipe(trim_column_values, pct=trim_pct)
-    )
-
-
-def get_xlabel(col):
-    """Returns customised x-axis label for column."""
+def monthly_savings(df):
+    """Plots histograms of savings account flows."""
+    pct_histplot = functools.partial(sns.histplot, stat='percent')
+    columns = ['sa_scaled_inflows', 'sa_scaled_outflows', 'sa_scaled_net_inflows']
     xlabels = {
-        "sa_outflows": "Outflows",
-        "sa_inflows": "Inflows",
-        "sa_net_inflows": "Net inflows",
+        "sa_scaled_outflows": "Outflows (% of monthly income)",
+        "sa_scaled_inflows": "Inflows (% of monthly income)",
+        "sa_scaled_net_inflows": "Net inflows (% of monthly income)",
     }
-    return xlabels[col] + " (% of monthly income)"
-
-
-def make_figure(df):
-    fig, ax = plt.subplots(2, 3)
     ylabel = "User-months (%)"
-    for i, col in enumerate(df.columns):
-        sns.histplot(x=df[col], stat="percent", ax=ax[0, i])
+
+    fig, ax = plt.subplots(2, 3)
+    for i, col in enumerate(columns):
+        data = df[col].pipe(hd.trim, pct=1, how='both')
+        pct_histplot(x=data, ax=ax[0, i])
         ax[0, i].set(xlabel="", ylabel=ylabel)
-        non_zero = df[col] != 0
-        sns.histplot(x=df[col][non_zero], stat="percent", ax=ax[1, i])
-        ax[1, i].set(xlabel=get_xlabel(col), ylabel=ylabel)
+        data = data[data != 0]
+        pct_histplot(x=data, ax=ax[1, i])
+        ax[1, i].set(xlabel=xlabel(col), ylabel=ylabel)
     fh.set_style()
     fh.set_size(fig, height=3)
     return fig, ax
 
 
-@hh.timer
-def main(df):
-    # data = make_data(df)
-    fig, ax = make_figure(df)
-    return fig
-
-
-if __name__ == "__main__":
-    args = fh.parse_args(sys.argv[1:])
-    df = ha.read_parquet(args.filepath)
-    fig = main(df)
-    fh.save_fig(fig, "monthly_savings.png")
+# if __name__ == "__main__":
+#     args = fh.parse_args(sys.argv[1:])
+#     df = ha.read_parquet(args.filepath)
+#     fig = main(df)
+#     fh.save_fig(fig, "monthly_savings.png")
