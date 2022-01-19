03d9cb54b2d13b8918b96d0dee39cedccf90b367
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-01-19 10:14:24 +0000

    Updates

--- entropy/analysis/make_analysis_data.py
+++ entropy/analysis/make_analysis_data.py
@@ -119,9 +119,11 @@ def constant_vars(df):
     df["log_income"] = np.log(df.income)
     cols = [
         "entropy_sptac",
+        "income",
         "log_income",
         "user_female",
         "age",
+        "region",
     ]
     return df.groupby(idx_cols)[cols].first()
 
@@ -143,11 +145,11 @@ def main(df):
     return data
 
 
-if __name__ == '__main__':
-    
-    SAMPLE = 'XX7'
-    fp_txn= f's3://3di-project-entropy/entropy_{SAMPLE}.parquet'
-    fp_analysis = f"s3://3di-project-entropy/analysis_data_{SAMPLE}.parquet"
+if __name__ == "__main__":
+
+    SAMPLE = "XX7"
+    fp_txn = f"s3://3di-project-entropy/entropy_{SAMPLE}.parquet"
+    fp_analysis = f"s3://3di-project-entropy/analysis_data.parquet"
     txn_data = ha.read_parquet(fp_txn)
     analysis_data = main(txn_data)
     ha.write_parquet(analysis_data, fp_analysis, index=True)
