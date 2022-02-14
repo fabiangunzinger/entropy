9d61cfa54f3ed524a7d6103a7aca6ea8978a2993
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-02-14 16:28:15 +0000

    Writeup

--- entropy/analysis/analysis.R
+++ entropy/analysis/analysis.R
@@ -39,19 +39,45 @@ m3 <- plm(has_sa_inflows ~ entropyz + month_spend + month_income + month,
           model = 'within',
 )
 
-m4 <- plm(has_sa_inflows ~ entropyz + month_income + spend_communication + spend_services + spend_finance + spend_motor + spend_travel + spend_hobbies + spend_other_spend + spend_household + spend_retail + user_id + month,
+m4 <- plm(has_sa_inflows ~ entropyz + month_income + spend_communication + spend_services + spend_finance + spend_motor + spend_travel + spend_hobbies + spend_household + spend_retail + spend_other_spend + month,
           data=df,
           index = c('user_id'),
           model = 'within',
 )
 
-
-stargazer(m1, m2, m3, m4,
-          title='Main results', 
-          out=file.path(TABDIR, 'main_results.tex'),
-          omit.stat=c("LL","ser","f"),
-          no.space=TRUE,
-          style = 'qje'
-          )
+stargazer(
+  m1, m2, m3, m4,
+  type = 'text',
+  title='Main results',
+  covariate.labels = c(
+    'Entropy',
+    'Month spend',
+    'Month income',
+    'Spend communication',
+    'Spend services',
+    'Spend finance',
+    'Spend motor',
+    'Spend travel',
+    'Spend hobbies',
+    'Spend household',
+    'Spend retail',
+    'Spend other'
+  ),
+  keep.stat = c('n', 'rsq'),
+  no.space = TRUE,
+  dep.var.labels.include = FALSE,
+  dep.var.caption = "Dependent variable: has transfers into savings account",
+  omit = '^month\\d+$',
+  add.lines = list(
+    c('Individual fixed effects', 'Yes', 'Yes', 'Yes', 'Yes'),
+    c('Month fixed effects', 'Yes', 'Yes', 'Yes', 'Yes')
+  ),
+  notes.align = 'l',
+  notes.append = FALSE,
+  notes.label = "",
+  notes = 'Note: ... *p<0.1; **p<0.05; ***p<0.01.',
+  font.size = 'normal',
+  out=file.path(TABDIR, 'main_results.tex')
+)
 
 
