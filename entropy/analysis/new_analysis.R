0f89f06a79b01fd60ffb789a6a44d9e3ddac5044
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-03-01 18:26:28 +0000

    Updated analysis

--- entropy/analysis/new_analysis.R
+++ entropy/analysis/new_analysis.R
@@ -9,18 +9,14 @@ library(modelsummary)
 library(plm)
 library(stargazer)
 
+# set fontsize of latex table produced by etable
 set_font = function(x, fontsize){
   if(missing(fontsize)) return(x)
-  
-  # like match.arg + extra info if error
   dreamerr::check_arg_plus(fontsize, "match(tiny, scriptsize, footnotesize, small, normalsize, large, Large)")
-  
   x[x == "%start:tab\n"] = paste0("\\begin{", fontsize, "}")
   x[x == "%end:tab\n"] = paste0("\\end{", fontsize, "}")
-  
-  x
+  return(x)
 }
-
 setFixest_etable(postprocess.tex = set_font)
 
 
@@ -28,26 +24,48 @@ TABDIR = '/Users/fgu/dev/projects/entropy/output/tables'
 
 dt = read_analysis_data()
 
-# todo: do in preproc 
-divider <- function(x){
-  return(x / 1000)
-}
-cols = grep('^month_(spend|income)', names(dt), value = T)
-dt[, (cols) := lapply(.SD, divider), .SDcols = cols]
 
-sw <- fixest::feols(has_sa_inflows ~ entropyz + sw0(has_reg_sa_inflows, prop_credit, month_spend, month_income, has_regular_income, has_month_income, has_benefits, has_pension, has_mortgage_pmt, has_rent_pmt, loan_funds, loan_repmt, pdloan_funds, pdloan_repmt) | user_id + month, data=dt, vcov = 'cluster')
+
+
+
+
+
+sw <- fixest::feols(has_sa_inflows ~ entropy + sw0(
+  # fin behaviour
+  has_reg_sa_inflows,
+  prop_credit, 
+  month_spend,
+  
+  # planning (not yet implemented)
+  
+  # hh characteristics
+  age,
+  is_urban,
+  month_income,
+  year_income,
+  has_regular_income,
+  has_month_income,
+  has_benefits,
+  has_pension,
+  has_mortgage_pmt,
+  has_rent_pmt,
+  loan_funds,
+  loan_repmt,
+  pdloan_funds,
+  pdloan_repmt
+  ) | user_id + month, data=dt, vcov = 'cluster')
 
 etable(
   sw, 
   se.below = T,
   title = 'Controls included one-by-one', 
-  tex = T,
+  # tex = T,
   fontsize = 'tiny',
   style.tex = style.tex(
     main = 'aer'
   ),
   label = 'tab:reg_sw0',
-  file=file.path(TABDIR, 'reg_sw0.tex'),
+  # file=file.path(TABDIR, 'reg_sw0.tex'),
   replace = T
 )
 
