fc7110c5e5cb4a764f110c4411d31fe31e36e0bf
Fabian Gunzinger <fa.gunzinger@gmail.com>
2022-03-02 17:24:00 +0000

    Edits

--- entropy/analysis/new_analysis.R
+++ entropy/analysis/new_analysis.R
@@ -7,7 +7,20 @@ library(estimatr)
 library(fixest)
 library(modelsummary)
 library(plm)
+library(purrr)
 library(stargazer)
+
+
+# Load data ------------------------------------------------------------------------
+
+dt = read_analysis_data()
+names(dt)
+
+
+# Global settings ------------------------------------------------------------------
+
+TABDIR = '/Users/fgu/dev/projects/entropy/output/tables'
+
 
 # set fontsize of latex table produced by etable
 set_font = function(x, fontsize){
@@ -19,70 +32,104 @@ set_font = function(x, fontsize){
 }
 setFixest_etable(postprocess.tex = set_font)
 
+setFixest_dict(c(
+  # entropy = "Entropy",
+  has_reg_sa_inflows = "Regular savings"
+))
 
-TABDIR = '/Users/fgu/dev/projects/entropy/output/tables' 
-
-dt = read_analysis_data()
-
-
-
-
-
-
-
-sw <- fixest::feols(has_sa_inflows ~ entropy + sw0(
-  # fin behaviour
-  has_reg_sa_inflows,
-  prop_credit, 
-  month_spend,
-  
-  # planning (not yet implemented)
-  
-  # hh characteristics
-  age,
-  is_urban,
-  month_income,
-  year_income,
-  has_regular_income,
-  has_month_income,
-  has_benefits,
-  has_pension,
-  has_mortgage_pmt,
-  has_rent_pmt,
-  loan_funds,
-  loan_repmt,
-  pdloan_funds,
-  pdloan_repmt
-  ) | user_id + month, data=dt, vcov = 'cluster')
-
-etable(
-  sw, 
+setFixest_etable(
   se.below = T,
-  title = 'Controls included one-by-one', 
+  style.tex = style.tex(
+    "base"
+  )
+)
+
+
+# Variables
+
+fin_behav = c(
+  'has_reg_sa_inflows',
+  'prop_credit',
+  'month_spend'
+  )
+
+planning = c()
+
+hh_chars = c(
+  'is_urban',
+  'month_income',
+  'has_regular_income',
+  'has_month_income',
+  'loan_repmt',
+  'pdloan_repmt'
+  )
+
+controls = c(fin_behav, planning, hh_chars)
+
+setFixest_fml(
+  ..entropy = 'entropy_tag_sst'
+)
+
+
+# FE specifications
+etable(
+  fixest::feols(xpd(
+    has_sa_inflows ~ ..entropy + ..controls | csw0(user_id, month),
+    ..controls = controls), data=dt
+  ), 
+  title = 'FE specifications', 
   # tex = T,
   fontsize = 'tiny',
-  style.tex = style.tex(
-    main = 'aer'
-  ),
-  label = 'tab:reg_sw0',
+  label = 'tab:reg_fe',
   # file=file.path(TABDIR, 'reg_sw0.tex'),
   replace = T
 )
 
 
-csw <- fixest::feols(has_sa_inflows ~ entropyz + csw0(has_reg_sa_inflows, prop_credit, month_spend, month_income, has_regular_income, has_month_income, has_benefits, has_pension, has_mortgage_pmt, has_rent_pmt, loan_funds, loan_repmt, pdloan_funds, pdloan_repmt) | user_id + month, data=dt, vcov = 'cluster')
-
-
+# Controls added one-by-one
 etable(
-  csw, 
-  se.below = T,
-  title = 'Main result', 
-  dict = c(
-    month_income="Month income"
-  ),
-  tex = T,
+  fixest::feols(has_sa_inflows ~ ..entropy + sw0(
+    # fin behaviour
+    has_reg_sa_inflows,
+    prop_credit,
+    month_spend,
+    # planning (not yet implemented)
+    # hh characteristics
+    is_urban,
+    month_income,
+    has_regular_income,
+    has_month_income,
+    loan_repmt,
+    pdloan_repmt
+  ) | user_id + month, data=dt, vcov = 'cluster'),
+  title = 'Controls included one-by-one', 
+  # tex = T,
+  # file=file.path(TABDIR, 'reg_sw0.tex'),
+  fontsize = 'tiny',
+  label = 'tab:reg_sw0',
+  replace = T
+)
+
+# Controls added cumulatively
+etable(
+  fixest::feols(has_sa_inflows ~ ..entropy + csw0(
+    # fin behaviour
+    has_reg_sa_inflows,
+    prop_credit, 
+    month_spend,
+    # planning (not yet implemented)
+    # hh characteristics
+    is_urban,
+    month_income,
+    has_regular_income,
+    has_month_income,
+    loan_repmt,
+    pdloan_repmt
+  ) | user_id + month, data=dt, vcov = 'cluster'), 
+  title = 'Controls included cumulatively', 
+  # tex = T,
+  # file=file.path(TABDIR, 'reg_csw0.tex'),
   fontsize = 'tiny',
   label = 'tab:reg_csw0',
-  file=file.path(TABDIR, 'reg_csw0.tex'),
   replace = T
 )
